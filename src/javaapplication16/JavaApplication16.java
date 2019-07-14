1900f5eba93ef7891121dfbf72f738f70ccfdcc8
User <User@User>
2019-07-14 17:52:08 -0400

    initial Commit

---
+++ src/javaapplication16/JavaApplication16.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication16;
+
+/**
+ *
+ * @author User
+ */
+public class JavaApplication16 {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Bonjour "+args[1]+", " +args[0]);
+    
+    }
+    
+}
